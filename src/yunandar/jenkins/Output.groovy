44b30cfaa2202150c0d16140e7417171e46af1d8
“yunandarz” <yunandarpalilati@gmail.com>
2024-02-21 14:02:11 +0800

    add steps in groovy function

--- src/yunandar/jenkins/Output.groovy
+++ src/yunandar/jenkins/Output.groovy
@@ -2,7 +2,7 @@ package yunandar.jenkins;
 
 class Output {
 
-    static def hello(String name) {
-        println("Hello ${name}")
+    static def hello(steps, String name) {
+        steps.echo("Hello ${name}")
     }
 }
